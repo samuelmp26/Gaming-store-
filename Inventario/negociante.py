b37a713178bc38ec48d51a38725af0cf1d3762b0
Sebastian Buritica Velasco <seburiticav@unal.edu.co>
2025-11-05 16:21:37 -0500

    Update negociante.py
    Updated negociante

--- Inventario/negociante.py
+++ Inventario/negociante.py
@@ -1,3 +1,13 @@
 class Negociante:
-    def __init__(self):
+    def _init_(self, margen_ganancia=0.2):
+        self.margen_ganancia = margen_ganancia
+        self.reglas_demanda = {}
+
+    def ofrecer_precio_compra(self, producto):
         pass
+
+    def ofrecer_precio_venta(self, producto):
+        pass
+
+    def simular_negociacion(self, producto, tipo="venta"):
+        pass
